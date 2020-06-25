adf71f29ede42111d32c6559a64f4e9912b71c86
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-06-25 21:41:53 +0700

    Menambahkan media query

--- App/Views/Templates/header.php
+++ App/Views/Templates/header.php
@@ -8,6 +8,226 @@
     <title><?= /** @var array $data */ $data['title'] ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= BASE_URL ?>Public/css/style.css">
+    <style>
+        @media (min-width: 992px) and (max-width: 1200px) {
+            main .container h1.display-4.font-weight-bold {
+                font-size: 50px;
+            }
+        }
+
+        @media (min-width: 768px) and (max-width: 992px) {
+            main .container h1.display-4.font-weight-bold {
+                font-size: 40px;
+            }
+
+            main .container #main table button.btn.badge {
+                margin: 0!important;
+                font-size: 15px;
+            }
+
+            main .container #main table a.badge {
+                margin: 0!important;
+                font-size: 15px;
+            }
+        }
+
+        @media (min-width: 576px) and (max-width: 768px) {
+            main .container h1.display-4.font-weight-bold {
+                font-size: 30px;
+            }
+
+            main .container #main table button.btn.badge {
+                margin: 0!important;
+                font-size: 15px;
+            }
+
+            main .container #main table a.badge {
+                margin: 0!important;
+                font-size: 13px;
+            }
+
+            main .container #main table td p {
+                font-size: 13px;
+            }
+
+            main .container #main table th {
+                font-size: 13px;
+            }
+
+            main .container p.lead {
+                font-size: 20px
+            }
+
+            main .container span.badge {
+                font-size: 15px;
+            }
+
+            main .container a.btn.btn-lg {
+                font-size: 18px;
+            }
+        }
+
+        @media (min-width: 425px) and (max-width: 576px) {
+            main .container h1.display-4.font-weight-bold {
+                font-size: 25px;
+            }
+
+            main .container h1.display-3.font-weight-bold {
+                font-size: 30px;
+            }
+
+            main .container p.lead {
+                font-size: 18px
+            }
+
+            main .container #main .card {
+                width: 400px!important;
+            }
+
+            main .container span.badge {
+                font-size: 13px;
+            }
+
+            main .container h4 {
+                font-size: 13px;
+            }
+
+            main .container a.btn.btn-lg {
+                font-size: 13px;
+            }
+
+            main .container #main table button.btn.badge {
+                margin: 0!important;
+                font-size: 13px;
+            }
+
+            main .container #main table a.badge {
+                margin: 0!important;
+                font-size: 13px;
+            }
+
+            main .container #main table td {
+                font-size: 13px;
+                padding: 8px!important;
+            }
+
+            main .container #main table th {
+                font-size: 13px;
+                padding: 8px!important;
+            }
+        }
+
+        @media (min-width: 320px) and (max-width: 425px) {
+            main .container h1.display-4.font-weight-bold {
+                font-size: 20px;
+            }
+
+            main .container h1.display-3.font-weight-bold {
+                font-size: 30px;
+            }
+
+            main .container p.lead {
+                font-size: 15px
+            }
+
+            main .container #main .card {
+                width: 300px!important;
+            }
+
+            main .container span.badge {
+                font-size: 10px;
+            }
+
+            main .container h3.mt-3 {
+                font-size: 25px;
+            }
+
+            main .container h4 {
+                font-size: 10px;
+            }
+
+            main .container a.btn.btn-lg {
+                font-size: 9px;
+            }
+
+            main .container #main table button.btn.badge {
+                margin: 0!important;
+                font-size: 9px;
+            }
+
+            main .container #main table a.badge {
+                margin: 0!important;
+                font-size: 9px;
+            }
+
+            main .container #main table td {
+                font-size: 9px;
+                padding: 6px!important;
+            }
+
+            main .container #main table th {
+                font-size: 9px;
+                padding: 6px!important;
+            }
+
+        }
+
+        @media (max-width: 320px) {
+            main .container h1.display-4.font-weight-bold {
+                font-size: 17px;
+            }
+
+            main .container p.lead {
+                font-size: 15px
+            }
+
+            main .container span.badge {
+                font-size: 10px;
+            }
+
+            main .container #main .card {
+                width: 275px!important;
+            }
+
+
+            main .container h4 {
+                font-size: 10px;
+            }
+
+            main .container a.btn.btn-lg {
+                font-size: 12px;
+            }
+
+            footer .container {
+                font-size: 15px;
+            }
+
+            main .container a.btn.btn-lg {
+                font-size: 9px;
+            }
+
+            main .container #main table button.btn.badge {
+                margin: 0!important;
+                font-size: 9px;
+            }
+
+            main .container #main table a.badge {
+                margin: 0!important;
+                font-size: 9px;
+            }
+
+            main .container #main table td {
+                font-size: 9px;
+                padding: 6px!important;
+            }
+
+            main .container #main table th {
+                font-size: 9px;
+                padding: 6px!important;
+            }
+        }
+
+    </style>
 </head>
 <body>
 <header>
